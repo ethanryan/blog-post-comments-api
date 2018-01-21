859ba2bf862ed446882bf3d92580dbc41b9235aa
ethanryan <ethanryan@gmail.com>
2018-01-21 16:49:07 -0500

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+p1 = Post.create([{username: 'bob', content: "bob's first post: hello world!"}])
+p2 = Post.create([{username: 'susan', content: "susan is in the house"}])
+
+
+
+c1 = Comment.create([{post_id: 1, username: 'frank', content: 'welcome bobby!'}])
+c2 = Comment.create([{post_id: 1, username: 'steve', content: 'hello world, indeed'}])
+
+c3 = Comment.create([{post_id: 2, username: 'bob', content: 'hi susan! welcome to this zany app'}])
