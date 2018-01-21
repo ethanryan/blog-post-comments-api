4ecbe61fcc1ae62b0b55d44dc370d5c2620189ec
ethanryan <ethanryan@gmail.com>
2018-01-21 17:01:54 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :posts, only: [:index, :create, :show]
+  
+  resources :comments, only: [:index, :create, :show, :update, :destroy]
+
 end
