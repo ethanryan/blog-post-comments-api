7f20b480b69e3df922a96becb1050ffb79882a93
ethanryan <ethanryan@gmail.com>
2018-01-21 20:04:39 -0500

    updated PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  # skip_before_action :verify_authenticity_token
+
   def index
     posts = Post.all
     render json: posts
@@ -8,6 +10,16 @@ class PostsController < ApplicationController
   def create
     post = Post.new
     render json: post
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+    render json: post
+  end
+
+  def post_params
+    params.require(:post).permit(:username, :content)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 
 p1 = Post.create([{username: 'bob', content: "bob's first post: hello world!"}])
 p2 = Post.create([{username: 'susan', content: "susan is in the house"}])
+p3 = Post.create([{username: 'satan lover', content: "all hail satan 666"}])
 
 
 
@@ -16,3 +17,7 @@ c1 = Comment.create([{post_id: 1, username: 'frank', content: 'welcome bobby!'}]
 c2 = Comment.create([{post_id: 1, username: 'steve', content: 'hello world, indeed'}])
 
 c3 = Comment.create([{post_id: 2, username: 'bob', content: 'hi susan! welcome to this zany app'}])
+
+c666 = Comment.create([{post_id: 3, username: 'sideshow mel', content: '6669'}])
+c6661 = Comment.create([{post_id: 3, username: 'bob', content: 'lol sideshow mel'}])
+c6662 = Comment.create([{post_id: 3, username: 'frank', content: 'this'}])
