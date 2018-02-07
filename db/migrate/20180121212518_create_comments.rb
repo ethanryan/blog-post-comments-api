d34f6830c3168560030eebbb7fc1e846f2af36cf
ethanryan <ethanryan@gmail.com>
2018-02-06 22:31:45 -0500

    added editable as boolean Comment attribute

--- db/migrate/20180121212518_create_comments.rb
+++ db/migrate/20180121212518_create_comments.rb
@@ -4,6 +4,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
       t.integer :post_id
       t.string :username
       t.string :content
+      t.integer :editable, :default => false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180121212518) do
+ActiveRecord::Schema.define(version: 20180206204700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180121212518) do
     t.integer "post_id"
     t.string "username"
     t.string "content"
+    t.integer "editable", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
